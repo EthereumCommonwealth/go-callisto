98a54cf780d25d1e3bc6ab1dd7fd19fb4d3e1933
DzikNsk <dkhodarenok@gmail.com>
2024-09-11 09:48:28 +0700

    English comments for MonetaryPolicy.sol, typo fixes

--- contracts/MonetaryPolicy/MonetaryPolicy.sol
+++ contracts/MonetaryPolicy/MonetaryPolicy.sol
@@ -5,31 +5,31 @@ pragma solidity ^0.8.16;
 
 contract MonetaryPolicy {
 
-    uint256 public MinerReward; // Награда для майнера за блок
-    uint256 public TreasuryReward; // Награда для трежери за блок
-    uint256 public StakeReward; // Награда для колдстака за блок
-    uint256 public ReserveReward; // Зарезервированный слот под награду, если понадобится распределение на уровне консенсуса
+    uint256 public MinerReward; // Miner reward per block
+    uint256 public TreasuryReward; // Treasury reward per block  
+    uint256 public StakeReward; // ColdStacking contract reward per block
+    uint256 public ReserveReward; // Reserved, in case we need some additional reward type per block
 
-    address public TreasuryAddress = 0x74682Fc32007aF0b6118F259cBe7bCCC21641600; // Адрес контракта Treasury
-    address public StakeAddress = 0x08A7c8be47773546DC5E173d67B0c38AfFfa4b84; // Адрес контракта ColdStaking
-    address public ReserveAddress = 0x0000000000000000000000000000000000000000; // Зарезервированный слот под адрес
+    address public TreasuryAddress = 0x74682Fc32007aF0b6118F259cBe7bCCC21641600; // Treasury contract address
+    address public StakeAddress = 0x08A7c8be47773546DC5E173d67B0c38AfFfa4b84; // ColdStaking contract address
+    address public ReserveAddress = 0x0000000000000000000000000000000000000000; // Reserved address
 
-    address public GovernanceDAO = 0x810059e1406dEDAFd1BdCa4E0137CbA306c0Ce36; // Владелец контракта
-    address public CallistoNetwork = 0xA9389DB4610175CaC4Fad32670A5189A89f874B5; // Владелец контракта
+    address public GovernanceDAO = 0x810059e1406dEDAFd1BdCa4E0137CbA306c0Ce36; // DAO vote address
+    address public CallistoNetwork = 0xA9389DB4610175CaC4Fad32670A5189A89f874B5; // Dexaran vote address
 
-    uint256 public TimeVoce = 45 days; // время отведенное для принятия решения по предложению вторым владельцем
-    uint256 public TimeEnd = 60 days; // время жизни предложения
+    uint256 public TimeVoice = 45 days; // time to wait for the second voter. After this period proposal will be accepted/declined based on the decision of the first voter.
+    uint256 public TimeEnd = 60 days; // how long each proposal will be active
 
     struct Data {
-        uint256 timeVoce; // крайний срок голосования для второго участника (если второй участник не проголосует до указанного времени, то первый участник может принять предложение в единоличном порядке)
-        uint256 timeEnd; // крайний срок жизни предложения (если второй учасник не проголосовал по предложению и первый участник не провел предложение в единоличном порядке до указанного времени, то предложение аннулируется)
-        bool governanceDAO; // активность участника (принимает true, если участник поддерживает предложение)
-        bool callistoNetwork; // активность участника (принимает true, если участник поддерживает предложение)
+        uint256 timeVoice; // deadline for the second voter. After this period vote will be accepted/declined based on the decision of the first voter.
+        uint256 timeEnd; // deadline for this propocal 
+        bool governanceDAO; // DAO vote 
+        bool callistoNetwork; // Dexaran vote
     }
 
-    mapping(bytes32 => Data) public proposals; // хеши предложений указывающие на структуру Data
+    mapping(bytes32 => Data) public proposals; // temporary storage for active proposals indexed by hashes
 
-    event Proposal(address indexed owner, bytes32 indexed hash, bool indexed voce); // логирование предложения
+    event Proposal(address indexed owner, bytes32 indexed hash, bool indexed voce); // new proposal event
 
     modifier onlyGovernanceDAO() {
         require(msg.sender == GovernanceDAO, "Only GovernanceDAO");
@@ -42,88 +42,88 @@ contract MonetaryPolicy {
     }
 
 
-    // Функция назначает награды за блок используемые в консенсусе ноды (вся эмиссия блока должна распределяться за 1 одну транзакцию)
-    function setRewards(uint256 _minerReward, uint256 _treasuryReward, uint256 _stakeReward, uint256 _reserveReward, bool _voce) external onlyTreasuryRecipients {
+    // sets rewards per block (all values must be updated in 1 TX)
+    function setRewards(uint256 _minerReward, uint256 _treasuryReward, uint256 _stakeReward, uint256 _reserveReward, bool _voice) external onlyTreasuryRecipients {
         bytes4 _selector = this.setRewards.selector;
-        bytes32 _hash = keccak256(abi.encodePacked(_selector, _minerReward, _treasuryReward, _stakeReward, _reserveReward)); // получаем хеш предложения
-        if(_consensus(_hash, _voce)){ // если консенсус достигнут
+        bytes32 _hash = keccak256(abi.encodePacked(_selector, _minerReward, _treasuryReward, _stakeReward, _reserveReward)); // proposal hash
+        if(_consensus(_hash, _voice)){ // если консенсус достигнут
             MinerReward = _minerReward;
             TreasuryReward = _treasuryReward;
             StakeReward = _stakeReward;
             ReserveReward = _reserveReward;            
         }
-        emit Proposal(msg.sender, _hash, _voce); // логируем предложение
+        emit Proposal(msg.sender, _hash, _voice); // proposal event for logging
     }
 
-    // Функция назначает новый адрес контракта TreasuryAddress
-    function setTreasuryAddress(address _treasuryAddress, bool _voce) external onlyTreasuryRecipients {
+    // function sets new Treasury Address
+    function setTreasuryAddress(address _treasuryAddress, bool _voice) external onlyTreasuryRecipients {
         bytes4 _selector = this.setTreasuryAddress.selector;
-        bytes32 _hash = keccak256(abi.encodePacked(_selector, _treasuryAddress)); // получаем хеш предложения
-        if(_consensus(_hash, _voce)){ // если консенсус достигнут
+        bytes32 _hash = keccak256(abi.encodePacked(_selector, _treasuryAddress)); // proposal hash
+        if(_consensus(_hash, _voice)){ // если консенсус достигнут
             TreasuryAddress = _treasuryAddress;
         }
-        emit Proposal(msg.sender, _hash, _voce); // логируем предложение
+        emit Proposal(msg.sender, _hash, _voice); // proposal event for logging
     }
 
-    // Функция назначает новый адрес контракта ColdStaking
+    // function sets new ColdStacking address
     function setStakeAddress(address _stakeAddress) external onlyGovernanceDAO {
         StakeAddress = _stakeAddress;
     }
 
-    // Функция назначает новый адрес для начисления резервной награды 
+    // function sets new "reserved for future" address
     function setReserveAddress(address _reserveAddress) external onlyGovernanceDAO {
         ReserveAddress = _reserveAddress;
     }
 
-    // Функция устанавливает новые сроки проведения голосования по предложению 
-    function setPeriods(uint256 _timeVoce, uint256 _timeEnd, bool _voce) external onlyTreasuryRecipients {
-        require((_timeVoce > 0) && (_timeEnd > _timeVoce));
+    // Updates time limits for future proposals
+    function setPeriods(uint256 _timeVoice, uint256 _timeEnd, bool _voice) external onlyTreasuryRecipients {
+        require((_timeVoice > 0) && (_timeEnd > _timeVoice));
         bytes4 _selector = this.setPeriods.selector;
-        bytes32 _hash = keccak256(abi.encodePacked(_selector, _timeVoce, _timeEnd)); // получаем хеш предложения
-        if(_consensus(_hash, _voce)){ // если консенсус достигнут
-            TimeVoce = _timeVoce;
+        bytes32 _hash = keccak256(abi.encodePacked(_selector, _timeVoice, _timeEnd)); // proposal hash
+        if(_consensus(_hash, _voice)){ // proposal accepted
+            TimeVoice = _timeVoice;
             TimeEnd = _timeEnd;
         }
-        emit Proposal(msg.sender, _hash, _voce); // логируем предложение
+        emit Proposal(msg.sender, _hash, _voice); // proposal event for logging
     }
 
-    // Функция назначает новый адрес контракта GovernanceDAO или CallistoNetwork
+    // Updates GovernanceDAO or Dexaran owner address. Sender must be current owner and can change only it's own address
     function setNewOwners(address _newOwner) external onlyTreasuryRecipients {
         require((_newOwner != GovernanceDAO) && (_newOwner != CallistoNetwork));
         (GovernanceDAO, CallistoNetwork) = msg.sender == GovernanceDAO ? (_newOwner, CallistoNetwork) : (GovernanceDAO, _newOwner);
     }
 
-    // Функция меняем адрес одного из владельцев, если этот владелец не опровергнет данного решения
-    function resetOwner(address _owner, address _newOwner, bool _voce) external onlyTreasuryRecipients {
+    // Updates GovernanceDAO or Dexaran owner address. Owners can update address of each other if other party did not decline such proposal
+    function resetOwner(address _owner, address _newOwner, bool _voice) external onlyTreasuryRecipients {
         require((_owner == GovernanceDAO) || (_owner == CallistoNetwork));
         require((_newOwner != GovernanceDAO) && (_newOwner != CallistoNetwork));
 
         bytes4 _selector = this.resetOwner.selector;
-        bytes32 _hash = keccak256(abi.encodePacked(_selector, _owner, _newOwner)); // получаем хеш предложения
-        if(_consensus(_hash, _voce)){ // если консенсус достигнут
+        bytes32 _hash = keccak256(abi.encodePacked(_selector, _owner, _newOwner)); // proposal hash
+        if(_consensus(_hash, _voice)){ // если консенсус достигнут
             (GovernanceDAO, CallistoNetwork) = _owner == GovernanceDAO ? (_newOwner, CallistoNetwork) : (GovernanceDAO, _newOwner);
         }
-        emit Proposal(msg.sender, _hash, _voce); // логируем предложение
+        emit Proposal(msg.sender, _hash, _voice); // proposal event for logging
     }
 
-    // Функция достижения консенсуса между владельцами (возвращает true если консенсус был достигнут)
-    function _consensus(bytes32 _hash, bool _voce) private returns (bool){
-        if (proposals[_hash].timeEnd < block.timestamp){ // срок жизни предложения истек (либо предложение не существовало), будем его пересоздавать
+    // this function calculates consensus, returns true if proposal accepted
+    function _consensus(bytes32 _hash, bool _voice) private returns (bool){
+        if (proposals[_hash].timeEnd < block.timestamp){ // proposal expired or never existed before, let's create it
             delete proposals[_hash];
-            if (!_voce) return (false); // предложение не создается, если голос "против"
-            proposals[_hash].timeVoce = block.timestamp + TimeVoce; // устанавливаем временной лимит для голосования второго владельца
-            proposals[_hash].timeEnd = block.timestamp + TimeEnd; // Устанавливаем срок жизни предложения
+            if (!_voice) return (false); // do nothing if we are declining proposal
+            proposals[_hash].timeVoice = block.timestamp + TimeVoice; // time limit for second voter
+            proposals[_hash].timeEnd = block.timestamp + TimeEnd; // proposal time limit
             (proposals[_hash].governanceDAO, proposals[_hash].callistoNetwork) = msg.sender == GovernanceDAO ? (true, false) : (false, true);
-        } else if (proposals[_hash].timeVoce < block.timestamp) { // период принятия одностороннего решения
+        } else if (proposals[_hash].timeVoice < block.timestamp) { // time limit for second voter expired, first voter can accept proposal without second party
             delete proposals[_hash];
             return (true);
-        } else { // период голосования
-            if (!_voce) { // голос "против" от любого из владельцев уничтожит предложение
+        } else { // still waiting for second voter
+            if (!_voice) { // proposal declined
                 delete proposals[_hash];
             } else {
-                (proposals[_hash].governanceDAO, proposals[_hash].callistoNetwork) = msg.sender == GovernanceDAO ? (true, proposals[_hash].callistoNetwork) : (proposals[_hash].governanceDAO, true); // выставляем голоса
-                if ((proposals[_hash].governanceDAO) && (proposals[_hash].callistoNetwork)) { // консенсус достигнут
-                    delete proposals[_hash]; // удаляем предложение, в нем более нет нужды
+                (proposals[_hash].governanceDAO, proposals[_hash].callistoNetwork) = msg.sender == GovernanceDAO ? (true, proposals[_hash].callistoNetwork) : (proposals[_hash].governanceDAO, true); // votes
+                if ((proposals[_hash].governanceDAO) && (proposals[_hash].callistoNetwork)) { // proposal accepted by both parties
+                    delete proposals[_hash]; // clean up
                     return (true);
                 }
             }
