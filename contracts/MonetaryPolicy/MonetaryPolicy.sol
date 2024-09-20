a4ca2cf84ee3b0f5d3dee0f635def294b8c7586d
Dexaran <dexaran820@gmail.com>
2024-09-20 15:35:20 +0100

    Comments clarifications and minor fixes.

--- contracts/MonetaryPolicy/MonetaryPolicy.sol
+++ contracts/MonetaryPolicy/MonetaryPolicy.sol
@@ -10,26 +10,26 @@ contract MonetaryPolicy {
     uint256 public StakeReward; // ColdStacking contract reward per block
     uint256 public ReserveReward; // Reserved, in case we need some additional reward type per block
 
-    address public TreasuryAddress = 0x74682Fc32007aF0b6118F259cBe7bCCC21641600; // Treasury contract address
-    address public StakeAddress = 0x08A7c8be47773546DC5E173d67B0c38AfFfa4b84; // ColdStaking contract address
-    address public ReserveAddress = 0x0000000000000000000000000000000000000000; // Reserved address
+    address public TreasuryAddress = 0x74682Fc32007aF0b6118F259cBe7bCCC21641600; // Treasury contract address.
+    address public StakeAddress = 0x08A7c8be47773546DC5E173d67B0c38AfFfa4b84;    // ColdStaking contract address.
+    address public ReserveAddress = 0x0000000000000000000000000000000000000000;  // Reserved address.
 
-    address public GovernanceDAO = 0x810059e1406dEDAFd1BdCa4E0137CbA306c0Ce36; // DAO vote address
-    address public CallistoNetwork = 0xA9389DB4610175CaC4Fad32670A5189A89f874B5; // Second party(Dexaran)  vote address
+    address public GovernanceDAO = 0x810059e1406dEDAFd1BdCa4E0137CbA306c0Ce36;   // DAO voting address.
+    address public CallistoNetwork = 0xA9389DB4610175CaC4Fad32670A5189A89f874B5; // Second party (Dexaran) voting address.
 
-    uint256 public TimeVoice = 45 days; // time to wait for the second voter. After this period proposal will be accepted/declined based on the decision of the first voter.
-    uint256 public TimeEnd = 60 days; // how long each proposal will be active
+    uint256 public TimeVoice = 45 days; // Time required for the second voter to cast their vote. After this period proposal CAN be accepted/declined based on the decision of the first voter.
+    uint256 public TimeEnd = 60 days;   // Lifetime of a proposal.
 
     struct Data {
-        uint256 timeVoice; // deadline for the second voter. After this period vote will be accepted/declined based on the decision of the first voter.
-        uint256 timeEnd; // deadline for this proposal 
-        bool governanceDAO; // DAO vote 
-        bool callistoNetwork; // Second party(Dexaran) vote
+        uint256 timeVoice;    // Deadline for the second voter. After this period the vote will be accepted/declined based on the decision of the first voter.
+        uint256 timeEnd;      // Deadline for this proposal.
+        bool governanceDAO;   // DAO vote.
+        bool callistoNetwork; // Second party (Dexaran) vote.
     }
 
-    mapping(bytes32 => Data) public proposals; // temporary storage for active proposals indexed by hashes
+    mapping(bytes32 => Data) public proposals; // Temporary storage for active proposals indexed by hashes.
 
-    event Proposal(address indexed owner, bytes32 indexed hash, bool indexed voice); // new proposal event
+    event Proposal(address indexed owner, bytes32 indexed hash, bool indexed voice); // New proposal event.
 
     modifier onlyGovernanceDAO() {
         require(msg.sender == GovernanceDAO, "Only GovernanceDAO");
@@ -87,13 +87,13 @@ contract MonetaryPolicy {
         emit Proposal(msg.sender, _hash, _voice); // proposal event for logging
     }
 
-    // Updates GovernanceDAO or Second party(Dexaran)  owner address. Sender must be current owner and can change only it's own address
+    // Updates GovernanceDAO or Second party (Dexaran) owner address. Sender must be current owner and can change only it's own address.
     function setNewOwners(address _newOwner) external onlyTreasuryRecipients {
         require((_newOwner != GovernanceDAO) && (_newOwner != CallistoNetwork));
         (GovernanceDAO, CallistoNetwork) = msg.sender == GovernanceDAO ? (_newOwner, CallistoNetwork) : (GovernanceDAO, _newOwner);
     }
 
-    // Updates GovernanceDAO or Second party(Dexaran)  owner address. Owners can update address of each other if other party did not decline such proposal
+    // Updates GovernanceDAO or Second party (Dexaran) owner address. Owners can update address of each other if other party did not decline such proposal.
     function resetOwner(address _owner, address _newOwner, bool _voice) external onlyTreasuryRecipients {
         require((_owner == GovernanceDAO) || (_owner == CallistoNetwork));
         require((_newOwner != GovernanceDAO) && (_newOwner != CallistoNetwork));
@@ -106,8 +106,8 @@ contract MonetaryPolicy {
         emit Proposal(msg.sender, _hash, _voice); // proposal event for logging
     }
 
-    // this function calculates consensus, returns true if proposal accepted
-    function _consensus(bytes32 _hash, bool _voice) private returns (bool){
+    // This function calculates consensus, returns true if the proposal is accepted.
+    function _consensus(bytes32 _hash, bool _voice) private returns (bool) {
         if (proposals[_hash].timeEnd < block.timestamp){ // proposal expired or never existed before, let's create it
             delete proposals[_hash];
             if (!_voice) return (false); // do nothing if we are declining proposal
